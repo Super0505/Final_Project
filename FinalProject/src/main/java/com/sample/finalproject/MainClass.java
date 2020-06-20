68d2ec809870e7b54533c511a5d1b310116e4d10
harry_du <a0917988537@gmail.com>
2020-06-20 13:04:00 +0800

    介面

--- FinalProject/src/main/java/com/sample/finalproject/MainClass.java
+++ FinalProject/src/main/java/com/sample/finalproject/MainClass.java
@@ -7,12 +7,13 @@ import java.io.FileWriter;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class MainClass {
 
   public static void main(String[] args) throws Exception {
-    File csv = new File("D:\\資料庫系統\\專題\\期末\\DB_students.csv"); // CSV資料檔案
+    File csv = new File("C:\\Database\\program\\火山矽肺症\\Final_Project\\DB_students.csv"); // CSV資料檔案
     BufferedReader reader = new BufferedReader(new FileReader(csv));// 要閱讀的最後一行
     Map<String, List<String>> map = new TreeMap<String, List<String>>();
     String line = reader.readLine();//read header
@@ -46,56 +47,32 @@ public class MainClass {
     }
     reader.close();
     fw.close();
+    Scanner scanner = new Scanner(System.in);
+	  while(true) {
+		  	System.out.print("Enter 1 is add\n 2 is remove\n3 is search s_list\n4 is search c_member\n5 is break");
+		    int num = scanner.nextInt();
+	  switch(num) {
+	  case 1:
+			    System.out.print("Enter a s_id:");
+			    String s_id = scanner.next();
+			    System.out.print("Enter a c_id:");
+			    String c_id = scanner.next();
+	  case 2:
+			    System.out.print("Enter a s_id:");
+			    String s_id1 = scanner.next();
+			    System.out.print("Enter a c_id:");
+			    String c_id1 = scanner.next();
+	  case 3:
+			    System.out.print("Enter a s_id:");
+			    String s_id2 = scanner.next();
+			    
+	  case 4:
+			    System.out.print("Enter a c_id:");
+			    String c_id2 = scanner.next();
+	  case 5:
+		  		break;
+	  	}
+	  }
   }
 
-//  private static String getField(String line) {
-//    return line.split(",")[0];// extract value you want to sort on
-//  }
-}
-
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package com.sample.finalproject;
-//
-//import java.io.BufferedReader;
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.util.StringTokenizer;
-//
-///**
-// *
-// * @author super
-// */
-//public class MainClass {
-//
-//  /**
-//   * @param args the command line arguments
-//   */
-//  public static void main(String[] args) {
-//    try {
-//      File csv = new File("D:\\資料庫系統\\專題\\期末\\DB_students.csv"); // CSV資料檔案
-//      BufferedReader br = new BufferedReader(new FileReader(csv));// 要閱讀的最後一行
-//      while (br.ready()) {
-//        String line = br.readLine();// 資料元素分為一行
-//        StringTokenizer st = new StringTokenizer(line, ",");
-//        String stnt = st.nextToken();//找學號
-//        String stdid = stnt.substring(1);
-//        int std = Integer.parseInt(stdid);//學號數字部分轉int
-//        while (st.hasMoreTokens()) {// 每個元素都顯示在一個製表符分隔的線
-//          System.out.print(st.nextToken() + ",");
-//        }
-//        System.out.println();
-//      }
-//      br.close();
-//    } catch (FileNotFoundException e) {// File物件的建立過程中的異常捕獲
-//      e.printStackTrace();
-//    } catch (IOException e) {// BufferedReader在關閉物件捕捉異常
-//      e.printStackTrace();
-//    }
-//  }
-//}
+  }
