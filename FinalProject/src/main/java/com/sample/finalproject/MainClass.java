c8d5967e2ac48b689886f75639765946c42040e3
Hsu_Chun_Wei <super20110505@gmail.com>
2020-06-29 11:01:58 +0800

    finish

--- FinalProject/src/main/java/com/sample/finalproject/MainClass.java
+++ FinalProject/src/main/java/com/sample/finalproject/MainClass.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -16,9 +15,9 @@ public class MainClass {
 
   public static void main(String[] args) throws Exception {
     int length = 0;
-    BufferedReader reader = new BufferedReader(new FileReader("DB_students.csv"));// 要閱讀的最後一行
+    BufferedReader reader = new BufferedReader(new FileReader("DB_students.csv"));
     Map<String, List<String>> map = new TreeMap<String, List<String>>();
-    String line = reader.readLine();// read header
+    String line = reader.readLine();
     while ((line = reader.readLine()) != null) {
       String key = line.split(",")[0];
       List<String> str = map.get(key);
@@ -30,13 +29,12 @@ public class MainClass {
     }
 
     FileWriter fw = new FileWriter("project.txt");
-    // System.out.println("student_id,course_id\n");
+    //System.out.println("student_id,course_id\n");
     for (List<String> list : map.values()) {
       for (String val : list) {
         length = val.length();
         fw.write(val);
         fw.write("\n");
-        //System.out.println(val);
       }
       for (int i = 0; i < 6; i++) {
         fw.write(".".repeat(length - 5) + "," + ".".repeat(4));
@@ -45,10 +43,12 @@ public class MainClass {
     }
     reader.close();
     fw.close();
-
+    Changer cc = new Changer();
+    Searcher ss = new Searcher();
     Scanner scanner = new Scanner(System.in);
     RandomAccessFile bf = new RandomAccessFile("project.txt", "rw");
-    while (true) {
+    boolean boo = true;
+    while (boo == true) {
       System.out.println("1. Add\n2. Remove\n3. Search Couerses by S_ID\n4. Search Students by C_ID\n5. Exit");
       int num = scanner.nextInt();
       switch (num) {
@@ -57,66 +57,109 @@ public class MainClass {
           String s_id = scanner.next();
           System.out.print("Enter a c_id:");
           String c_id = scanner.next();
-          addCourse(s_id, c_id, bf);
+          System.out.println("Add...");
+          cc.addCourse(s_id, c_id, bf);
           break;
         case 2:
           System.out.print("Enter a s_id:");
           String s_id1 = scanner.next();
           System.out.print("Enter a c_id:");
           String c_id1 = scanner.next();
-          deleteCourse(s_id1, c_id1, bf);
+          System.out.println("Delete...");
+          cc.deleteCourse(s_id1, c_id1, bf);
           break;
         case 3:
           System.out.print("Enter a s_id:");
           String s_id2 = scanner.next();
-          search_S(s_id2, bf);
+          System.out.println("Search...");
+          ss.search_S(s_id2, bf);
           break;
         case 4:
           System.out.print("Enter a c_id:");
           String c_id2 = scanner.next();
-          search_C(c_id2, bf);
+          System.out.println("Search...");
+          ss.search_C(c_id2, bf);
           break;
         case 5:
+          boo = false;
           break;
       }
     }
   }
+}
 
-  private static void addCourse(String s_id, String c_id, RandomAccessFile reader) throws IOException {
+class Changer {
+
+  Changer() {
+
+  }
+
+  public Boolean addCourse(String s_id, String c_id, RandomAccessFile reader) throws IOException {
     String line;
+    String match = s_id + "," + c_id;
+    boolean find = false;
+    reader.seek(0);
+    long size = -1;
     while ((line = reader.readLine()) != null) {
-      String key = line.split(",")[0];
-      if (key.equalsIgnoreCase(s_id)) {
-        reader.readLine();
-        reader.writeBytes(s_id + "," + c_id);
+      if (s_id.equalsIgnoreCase(line.split(",")[0])) {
+        if (line.equalsIgnoreCase(match)) {
+          System.out.println("你不會影分身，不能衝堂");
+          return false;
+        } else {
+          find = true;
+          size = reader.getFilePointer();
+        }
       }
     }
-
+    if (find) {
+      reader.seek(size);
+      reader.writeBytes(s_id + "," + c_id);
+    }
+    return true;
   }
 
-  private static void deleteCourse(String s_id, String c_id, RandomAccessFile reader) throws IOException {
-  }
-
-  public static void search_S(String s_id, RandomAccessFile reader) throws IOException {
+  public void deleteCourse(String s_id, String c_id, RandomAccessFile reader) throws IOException {
     String line;
+    reader.seek(0);
     while ((line = reader.readLine()) != null) {
       String key = line.split(",")[0];
       if (key.equalsIgnoreCase(s_id)) {
+        String value = line.split(",")[1];
+        if (value.equalsIgnoreCase(c_id)) {
+          reader.seek(reader.getFilePointer() - line.length() - 1);
+          reader.writeBytes(".".repeat(key.length()) + "," + ".".repeat(4));
+        }
+      }
+    }
+  }
+}
+
+class Searcher {
+
+  Searcher() {
+
+  }
+
+  public void search_S(String s_id, RandomAccessFile reader) throws IOException {
+    String line;
+    reader.seek(0);
+    while ((line = reader.readLine()) != null) {
+      if (s_id.equalsIgnoreCase(line.split(",")[0])) {
         System.out.println(line);
       }
     }
   }
 
-  public static void search_C(String c_id, RandomAccessFile reader) throws IOException {
+  public void search_C(String c_id, RandomAccessFile reader) throws IOException {
     String line;
-    int i = 0;
+    reader.seek(0);
+    int total = 0;
     while ((line = reader.readLine()) != null) {
-      String key = line.split(",")[1];
-      if (key.equals(c_id)) {
-        //System.out.println(line);
-        i++;
+      if (c_id.equalsIgnoreCase(line.split(",")[1])) {
+        System.out.println(line.split(",")[0]);
+        total++;
       }
     }
-    System.out.println("人數: " + i + "人");
+    System.out.println("人數: " + total + "人");
   }
 }
